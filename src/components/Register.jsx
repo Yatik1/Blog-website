6c1e071680d88d780f16378b1b9354ba3cbcd453
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-07-02 17:41:55 +0530

    done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,14 +1,36 @@
+import { useState } from 'react';
 import React from 'react'
 import './Val.css'
 import { Link } from 'react-router-dom'
 
 
 function Register() {
+  
+   const [username, setUsername] = useState('');
+   const [password , setPassword] = useState('');
+
+   async function reg(ev){
+       ev.preventDefault();
+       await fetch('http://localhost:5000/register' , {
+           method: 'POST',
+           body: JSON.stringify({username ,password}),
+           headers: {'Content-Type': 'application/json'},
+       })
+   }
+
   return (
-    <form className='register'>
+    <form className='register' onSubmit={reg}>
       <h1>Register</h1>
-    <input type="text" placeholder='username' />
-    <input type="password" placeholder='password' />
+    <input type="text" 
+            placeholder='username' 
+            value={username} 
+            onChange={e=> setUsername(e.target.value)}/>
+
+    <input type="password"
+           placeholder='password'
+            value={password} 
+            onChange={e => setPassword(e.target.value)}/>
+            
      <button>Register</button>
 
      <footer>
